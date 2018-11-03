ac96c818f78ed49ccad9f68de03817c0c2bad15c
Patricio <patricioprp06@gmail.com>
2018-11-03 20:33:02 -0300

    herramienta para debug y autenticacion

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //Mis Aplicaciones
+
+        Barryvdh\Debugbar\ServiceProvider::class,
     ],
 
     /*
@@ -226,6 +229,9 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        //Mis Aplicaciones
+   
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Agente de Viaje Inteligente
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href=""></a>
+                    <a href="">Turismo</a>
+                    <a href="">Conciertos</a>
+                    <a href="">Diversion</a>
+                    <a href="">Amigos</a>
                 </div>
             </div>
         </div>
